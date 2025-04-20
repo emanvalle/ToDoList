4a32f56fc16f81a70e83b9977d86e4ab30f6b64e
Eric Valle <emanvalle@gmail.com>
2025-04-19 23:48:08 -0700

    4-19-25 - 3 Changes - Developed RegisterView - Modified HeaderView for code reusability - Modified header code in LoginView

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -17,7 +17,10 @@ struct LoginView: View {
         NavigationView {
             VStack {
                 //Header
-                HeaderView()
+                HeaderView(title: "To Do List",
+                           subtitle: "Get things done",
+                           angle: 15,
+                           background: .pink)
                 
                 //Login Form
                 Form {
@@ -41,6 +44,7 @@ struct LoginView: View {
                     }
                     .padding()
                 }
+                .offset(y: -50)
                 
                 //Create Acct
                 VStack {
